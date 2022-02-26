aad6c2bec0bd84f86dc317628ad446db57d6c842
Natheul <59397202+Thuleanx@users.noreply.github.com>
2022-02-26 14:35:23 -0600

    Sample Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour {
+	public void OnMovement(InputValue value) {
+		Vector2 mvmt = value.Get<Vector2>();
+		if (mvmt.x == 0 || mvmt.y == 0)
+			transform.Translate(new Vector3(mvmt.x, 0, mvmt.y));
+	}
+}
