087117806be2f2d476862c39941009d439064c40
thomastdbui <thomastdbui@gmail.com>
2022-03-05 15:31:22 -0500

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,19 +3,8 @@ using System;
 using UnityEngine.InputSystem;
 
 public class Enemy : MonoBehaviour {
-	// public void OnMovement(InputValue value) {
-	// 	Vector2 mvmt = value.Get<Vector2>();
-	// 	if (mvmt.x == 0 || mvmt.y == 0)
-	// 		transform.Translate(new Vector3(mvmt.x, 0, mvmt.y));
-	// 		this.die(value);
 
-	// }
-	// // public void OnMovement(InputValue value) {
-	// // 	Vector2 mvmt = value.Get<Vector2>();
-	// // 	if (mvmt.x == 0 || mvmt.y == 0)
-	// // 		transform.Translate(new Vector3(mvmt.x, 0, mvmt.y));
-	// // }
-
+	
     public void die(InputValue value) {
         // Kills the game object
         Destroy (gameObject);
@@ -24,6 +13,7 @@ public class Enemy : MonoBehaviour {
         // Removes the rigidbody from the game object
         Destroy(GetComponent<Rigidbody>());
 	}
+
     //     // Kills the game object in 5 seconds after loading the object
     //     // Destroy (gameObject, 5);
     //     // When the user presses Ctrl, it will remove the script 
@@ -33,10 +23,10 @@ public class Enemy : MonoBehaviour {
 
     //  When the user presses Ctrl, it will remove the script 
     //  named FooScript from the game object
-     public void Update (InputValue input) {
-         if (Input.GetButton ("Fire1") && GetComponent (FooScript))
-            //  Destroy (GetComponent (FooScript));
-            Console.WriteLine("This is C#");
-			this.die(input);
-     }
+    //  public void Update (InputValue input) {
+    //      if (Input.GetButton ("Fire1") && GetComponent (FooScript))
+    //         //  Destroy (GetComponent (FooScript));
+    //         Console.WriteLine("This is C#");
+	// 		// this.die(input);
+    //  }
 }
