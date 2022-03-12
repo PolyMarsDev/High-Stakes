1938c1dc6b7974e1668c828f7922c9bb56e793c2
Natheul <59397202+Thuleanx@users.noreply.github.com>
2022-03-12 14:27:09 -0600

    Fix rotation

--- High-Stakes/Assets/Scripts/Grid/CustomGrid.cs
+++ High-Stakes/Assets/Scripts/Grid/CustomGrid.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+enum UIIndicator {
+	Movable,
+	Attackable
+}
+
 public class CustomGrid : MonoBehaviour {
 	public Tilemap groundGrid;
 	public Tilemap UIgrid;
 
-
 	// Raycast Detection
 	public Vector3 GetMouseWorldPosition() {
 		return Vector3.zero;
+	}
+
+	public void SetUIIndicator() {
 	}
 }
