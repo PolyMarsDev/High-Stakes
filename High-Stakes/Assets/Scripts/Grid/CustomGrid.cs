3d9d5e048c30f28f994a5b5c782db224aaaa7d5c
Natheul <59397202+Thuleanx@users.noreply.github.com>
2022-03-12 14:46:42 -0600

    Custom grid + Mouse raycast to cell

--- High-Stakes/Assets/Scripts/Grid/CustomGrid.cs
+++ High-Stakes/Assets/Scripts/Grid/CustomGrid.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
-
-enum UIIndicator {
-	Movable,
-	Attackable
-}
+using UnityEngine.InputSystem;
 
 public class CustomGrid : MonoBehaviour {
 	public Tilemap groundGrid;
 	public Tilemap UIgrid;
 
+	public enum UIIndicator {
+		None,
+		Movable,
+		Attackable,
+	}
+
 	// Raycast Detection
 	public Vector3 GetMouseWorldPosition() {
+		Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+		Plane plane = new Plane();
+		plane.SetNormalAndPosition(Vector3.up, transform.position);
+		float dist;
+		if (plane.Raycast(ray, out dist)) {
+
+		}
 		return Vector3.zero;
 	}
 
-	public void SetUIIndicator() {
+	public void SetUIIndicator(Vector2Int position, UIIndicator indicator) {
+		if (indicator == UIIndicator.None)
+			UIgrid.SetTile((Vector3Int) position, null);
 	}
 }
