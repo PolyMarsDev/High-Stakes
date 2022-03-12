9de6545156d09094388e51432786c52d974d719c
Natheul <59397202+Thuleanx@users.noreply.github.com>
2022-03-12 13:59:12 -0600

    Testing with 2d sprite + Camera change

--- High-Stakes/Assets/Scripts/Behaviours/CircularRotationOnInput.cs
+++ High-Stakes/Assets/Scripts/Behaviours/CircularRotationOnInput.cs
@@ -11,21 +11,21 @@ public class CircularRotationOnInput : MonoBehaviour {
 		float rotate = value.Get<float>();
 		if (rotate != 0) {
 			desiredRotation += (int) rotate;
-			if (desiredRotation < 0) desiredRotation += 4;
-			if (desiredRotation > 4) desiredRotation -= 4;
+			// if (desiredRotation < 0) desiredRotation += 4;
+			// if (desiredRotation > 4) desiredRotation -= 4;
 		}
 	}
 
 	void Update() {
 		float desiredLocationAngle = desiredRotation * 90;
 		// Because math on angles is annoying
-		if (Mathf.Abs(currentRotationAngle + 360 - desiredLocationAngle) < 
-			Mathf.Abs(currentRotationAngle - desiredLocationAngle)) {
-			currentRotationAngle += 360;
-		} else if (Mathf.Abs(currentRotationAngle - 360 - desiredLocationAngle) < 
-			Mathf.Abs(currentRotationAngle - desiredLocationAngle)) {
-			currentRotationAngle -= 360;
-		}
+		// if (Mathf.Abs(currentRotationAngle + 360 - desiredLocationAngle) < 
+		// 	Mathf.Abs(currentRotationAngle - desiredLocationAngle)) {
+		// 	currentRotationAngle += 360;
+		// } else if (Mathf.Abs(currentRotationAngle - 360 - desiredLocationAngle) < 
+		// 	Mathf.Abs(currentRotationAngle - desiredLocationAngle)) {
+		// 	currentRotationAngle -= 360;
+		// }
 		currentRotationAngle = Calc.Damp(currentRotationAngle, desiredLocationAngle, dampAlpha, Time.deltaTime);
 		transform.rotation = Quaternion.Euler(0, currentRotationAngle, 0);
 	}
--- High-Stakes/Assets/Scripts/CameraFacingBillboard.cs
+++ High-Stakes/Assets/Scripts/CameraFacingBillboard.cs
@@ -4,7 +4,14 @@ using System.Collections.Generic;
  
 public class CameraFacingBillboard : MonoBehaviour
 {
-    public Camera m_Camera;
+    public Camera m_Camera {get; private set; }
+
+	// Change to automatically detect camera in scene
+	void Awake() {
+		m_Camera = Camera.main;
+		if (m_Camera == null)
+			m_Camera = FindObjectOfType<Camera>();
+	}
  
     //Orient the camera after all movement is completed this frame to avoid jittering
     void LateUpdate()
