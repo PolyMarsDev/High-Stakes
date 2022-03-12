7b5448ebbd77b82be9784bae8ee07680edb6b1fc
Allison Yeh <63689595+allisonkyeh@users.noreply.github.com>
2022-03-12 14:40:07 -0500

    env mockup, billboard script

---
+++ High-Stakes/Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+ 
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera m_Camera;
+ 
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+}
+
+
