e30fb12020360a65177f6df901c62f36f6a9d2e9
thomastdbui <thomastdbui@gmail.com>
2022-03-19 15:45:34 -0400

    Update Peasant.cs

--- High-Stakes/Assets/Scripts/Peasant.cs
+++ High-Stakes/Assets/Scripts/Peasant.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Peasant : MonoBehaviour
+public class Peasant : Unit
 {
     // Start is called before the first frame update
     void Start()
@@ -10,9 +10,24 @@ public class Peasant : MonoBehaviour
         
     }
 
+
+	public override List<Vector2Int> GetMoveTo() {
+		int[] dx = {1, -1, 0, 0};
+		int[] dy = {0, 0, -1, 1};
+
+		List<Vector2Int> adjacents = new List<Vector2Int>();
+		for (int k = 0; k < 4; k++) {
+			Vector2Int nxtPos = pos + Vector2Int.right * dx[k] + Vector2Int.up * dy[k];
+			if (CustomGrid.Instance.CanMoveTo(nxtPos)) adjacents.Add(nxtPos);
+		}
+		return adjacents;
+	}
+
+
     // Update is called once per frame
     void Update()
     {
         
     }
+
 }
