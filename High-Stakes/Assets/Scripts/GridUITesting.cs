5200ecf080f2776026b9fe91503d3056bc89829d
jakobwismar <51007356+PolyMarsDev@users.noreply.github.com>
2022-03-19 14:58:48 -0400

    grid ui

--- High-Stakes/Assets/Scripts/GridUITesting.cs
+++ High-Stakes/Assets/Scripts/GridUITesting.cs
@@ -20,4 +20,5 @@ public class GridUITesting : MonoBehaviour
         Vector3Int position = gridUI.UIGrid.WorldToCell(new Vector3(player.transform.position.x, 0, player.transform.position.z));
         gridUI.addIndicator(tile, new Vector2Int(position.x, position.y));
     }
+    
 }
