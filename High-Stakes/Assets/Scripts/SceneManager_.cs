6c04b22e6e12b6df064402d77d89df3f8c7ba9b3
jakobwismar <51007356+PolyMarsDev@users.noreply.github.com>
2022-04-16 13:43:05 -0400

    SceneManager

---
+++ High-Stakes/Assets/Scripts/SceneManager_.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SceneManager_ : MonoBehaviour
+{
+    public Image overlay;
+
+    public void Start()
+    {
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 1.0f);
+        StartCoroutine(FadeIn());
+    }
+    public void LoadScene(int index)
+    {
+        StartCoroutine(FadeToScene(index));
+    }
+    public void ReloadScene()
+    {
+        LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public IEnumerator FadeToScene(int index)
+    {
+        for (float i = 0f; i <= 1f; i += 0.01f)
+        {
+            overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, Mathf.SmoothStep(0.0f, 1.0f, i));
+            yield return new WaitForSeconds(.01f);
+        }
+        SceneManager.LoadScene(index);
+    }
+
+    public IEnumerator FadeIn()
+    {
+        for (float i = 0f; i <= 1f; i += 0.01f)
+        {
+            overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, Mathf.SmoothStep(1.0f, 0.0f, i));
+            yield return new WaitForSeconds(.01f);
+        }
+        ReloadScene();
+    }
+}
