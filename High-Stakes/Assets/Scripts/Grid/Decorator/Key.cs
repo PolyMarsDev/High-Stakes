e97632f59c1ace48c7c318c7b2988562c6ae7686
Natheul <59397202+Thuleanx@users.noreply.github.com>
2022-04-23 14:41:30 -0500

    FMOD integration start

--- High-Stakes/Assets/Scripts/Grid/Decorator/Key.cs
+++ High-Stakes/Assets/Scripts/Grid/Decorator/Key.cs
@@ -2,6 +2,5 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Key : Unit {
-	public override List<Vector2Int> GetMoveTo() => new List<Vector2Int>();
+public class Key : MonoBehaviour {
 }
