918bba7b2ef18caf576079e8aa644e21b0ab2761
jakobwismar <51007356+PolyMarsDev@users.noreply.github.com>
2022-05-07 14:06:20 -0400

    !

--- High-Stakes/Assets/Scripts/UI/EnemyInfoDisplayTracker.cs
+++ High-Stakes/Assets/Scripts/UI/EnemyInfoDisplayTracker.cs
@@ -37,7 +37,14 @@ namespace UI {
 						GetNewTrackers(enemy);
 					}
 					currentlyTracking = enemy;
+<<<<<<< HEAD
 				} else ClearTrackers();
+=======
+				} else {
+					ClearTrackers();
+					currentlyTracking = null;
+				}
+>>>>>>> e6c98e3773fe035ae478a3b940fa4c04f7ca4709
 			} 
 		}
 
@@ -50,13 +57,22 @@ namespace UI {
 		public void GetNewTrackers(Enemy enemy) {
 			foreach (var pos in enemy.GetAttackablePositions()) {
 				UITrackingObjects.Add(
+<<<<<<< HEAD
 					GridUI.Instance.AddIndicator(GridUI.Indicator.MOVABLE, GridUI.GridToUI(pos)
 					- Vector3.forward * .2f) // this is neccessary to prevent z-fighting
+=======
+					GridUI.Instance.AddIndicator(GridUI.Indicator.ATTACKABLE, GridUI.GridToUI(pos)
+					- Vector3.forward * .3f) // this is neccessary to prevent z-fighting
+>>>>>>> e6c98e3773fe035ae478a3b940fa4c04f7ca4709
 				);
 			}
 			foreach (var pos in enemy.GetVisionPositions()) {
 				UITrackingObjects.Add(
+<<<<<<< HEAD
 					GridUI.Instance.AddIndicator(GridUI.Indicator.MOVABLE, GridUI.GridToUI(pos)
+=======
+					GridUI.Instance.AddIndicator(GridUI.Indicator.VISIBLE, GridUI.GridToUI(pos)
+>>>>>>> e6c98e3773fe035ae478a3b940fa4c04f7ca4709
 					- Vector3.forward * .2f) // this is neccessary to prevent z-fighting
 				);
 			}
